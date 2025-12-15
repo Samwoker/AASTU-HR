3d770ff7d51651c717f82950325e8d1ee8a455ed
Samwoker <samwoker112@gmail.com>
2025-12-15 06:10:18 +0300

    feat: update color palette for consistency; enhance App component with authentication handling and replace ToastContainer with ToastProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,32 @@
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import AppRoutes from "./routes";
 import { SidebarProvider } from "./app/context/SidebarContext";
+import ToastProvider from "./app/components/common/ToastProvider";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useAuthSlice, authActions } from "./app/slice/authSlice";
+import { selectAuthToken } from "./app/slice/authSlice/selectors";
+
+function AuthBootstrapper() {
+  useAuthSlice();
+  const dispatch = useDispatch();
+  const token = useSelector(selectAuthToken);
+
+  useEffect(() => {
+    if (!token) return;
+    dispatch(authActions.getMeRequest());
+  }, [dispatch, token]);
+
+  return null;
+}
 
 function App() {
   return (
     <BrowserRouter>
       <SidebarProvider>
+        <AuthBootstrapper />
         <AppRoutes />
-        <ToastContainer />
+        <ToastProvider />
       </SidebarProvider>
     </BrowserRouter>
   );
